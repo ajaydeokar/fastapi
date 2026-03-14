7cc06ef27ab41f0fa9b580e3079560406b2d051f
unknown <ajaydeokarj@gmail.com>
2026-03-15 00:13:27 +0530

    Revert "Some more changes"
    This reverts commit 82b8d8b3b33f8723aebabc1e4e3a7268b2ea90ab.
    
    REvert comments

--- main.py
+++ main.py
@@ -149,7 +149,7 @@ def add_item(item: Item, response_model=ItemResponse):
     )
 
 def main():
-    print("Hello from fastapicode branch fastapi some !")
+    print("Hello from fastapicode branch fastapi!")
 
 if __name__ == "__main__":
     main()
